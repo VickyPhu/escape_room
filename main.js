29faf338cda4278796d827ead6237359fb3808cc
VickyPhu <victoriaphu@gmail.com>
2024-11-09 21:54:25 +0100

    start scene - image, h1 & start button

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 window.addEventListener('DOMContentLoaded', main);
 
 function main() {
-    
+    loadStartScene()
+}
+
+function loadStartScene() {
+    const startScene = document.createElement('img');
+    startScene.src ="images/haunted_house.webp";
+
+    const gameName = document.createElement('h1');
+    gameName.textContent = 'Vanished in Silence';
+    gameName.classList.add('h1');
+
+    const button = document.createElement('button');
+    button.textContent = 'Start';
+    button.onclick = loadPremiseScene;
+    button.classList.add('startButton');
+
+    sceneContainer.append(startScene, gameName, button);
 }
